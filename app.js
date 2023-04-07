ae54e8c6f322abe8e62615a8d59040a175069bcd
Nai <62731284+tobiastka@users.noreply.github.com>
2023-04-07 10:14:25 -0300

    Server/DB
    Se agrego la autenticación de la db para chequear que todo ande correctamente así como la sincronización de las tablas

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 import express from 'express'
 import routerManga from './routes/mangaRoutes.js'
+import dotenv from 'dotenv'
+import { sequelizeConnection } from './db.js'
+// ----- MODELOS ------>
+import './models/mangaModel.js'
+import { initialDataManga } from './data/updateDB.js'
+import bodyParser from 'body-parser'
+
+dotenv.config()
 
 const app = express()
-
 const PORT = process.env.PORT || 3000
+sequelizeConnection.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos establecida con éxito.')
 
-app.listen(PORT, () => {
-  console.log('Server ON, on port:', PORT)
-})
+    sequelizeConnection.sync({ force: true })
+      .then(() => {
+        console.log('Tablas creadas correctamente')
+        app.listen(PORT, () => {
+          console.log('Server ON, on port:', PORT)
+          initialDataManga()
+        })
+      })
+      .catch((error) => {
+        console.log('Error al crear las tablas:', error)
+      })
+  })
+  .catch((error) => {
+    console.log('Error al conectar con la base de datos:', error)
+  })
 
+app.use(bodyParser.json())
 app.use('/manga', routerManga)
---
+++ db.js
@@ -0,0 +1,10 @@
+import Sequelize from 'sequelize'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS, DB_DIALECT } = process.env
+export const sequelizeConnection = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+  host: DB_HOST,
+  dialect: DB_DIALECT,
+  PORT: DB_PORT
+})
