692f23dadee4b6633d8e395ce395b3da275ac64a
Nai <62731284+tobiastka@users.noreply.github.com>
2023-04-10 16:58:10 -0300

    Loggin
    Se desactivo el loggin de consola

--- db.js
+++ db.js
@@ -6,5 +6,6 @@ const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS, DB_DIALECT } = process.env
 export const sequelizeConnection = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
   dialect: DB_DIALECT,
-  PORT: DB_PORT
+  PORT: DB_PORT,
+  logging: false
 })
