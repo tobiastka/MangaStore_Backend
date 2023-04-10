568109e23af6d91e89bf98bfc7a91d1b3b153ac2
Nai <62731284+tobiastka@users.noreply.github.com>
2023-04-10 17:00:27 -0300

    Rutas coleccion
    Se creo la primer ruta para las colecciones para crearlas, asi como un test para el endpoint

---
+++ routes/collectionRoutes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import { postCollectionController } from '../controllers/collectionControllers.js'
+
+const routerCollection = Router()
+
+routerCollection.post('/', postCollectionController)
+export default routerCollection
+/*
+Test Call
+{
+    "nombre":"Tokyo Ghoul",
+    "autor":"Sui Ishida",
+    "cantidadVolumenes":14,
+    "formato":"Tankoubon",
+    "imagenBanner":"https://images.fanart.tv/fanart/tokyo-ghoul-615ea53b29fe5.png",
+    "resumenHistoria":"Ken Kaneki es un estudiante tímido y recluido que queda obnubilado cuando la hermosa Rize lo invita a salir. Lo que no sabe es que Rize guarda un peligroso secreto: Es una ghoul y sólo le interesa comérselo. Tras un rescate fallido, Ken se convierte en el primer híbrido ghoul-humano, y se ve forzado a sumergirse en el violento y oscuro mundo de los Ghouls si quiere sobrevivir a su nueva e insólita condición.",
+    "genero":"Seinen"
+}
+
+*/
